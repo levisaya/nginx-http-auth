48cdb3cd10a05d99c862c0ad0f08b7f69929e6bd
Andy Levisay <levisaya@gmail.com>
2015-10-08 10:49:02 -0500

    Session handling
    Added some session support so the user isn't authenticated on every
    request.

--- nginx_http_auth/auth_backend.py
+++ nginx_http_auth/auth_backend.py
@@ -3,9 +3,10 @@ import os
 import click
 import logging
 logging.basicConfig()
-import traceback
+
 
 local_dir = os.path.abspath(os.path.join(__file__, '..'))
+
 
 @click.command()
 @click.option('--config_module', default='authorizers.test_auth', help='Import of the configuration module.')
@@ -15,27 +16,44 @@ local_dir = os.path.abspath(os.path.join(__file__, '..'))
               default=os.path.join(local_dir, 'templates', 'login_form.html'),
               help='Path to the template to render.')
 def server(config_module, port, secret_key, template):
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder=os.path.abspath(os.path.join(__file__, '..', 'templates')))
 
     config = __import__(config_module, globals(), locals(), ['object'], 0)
 
     @app.route('/auth-proxy', methods=['GET'])
     def auth():
-        if 'nginxauth' in session:
-            username, password = session['nginxauth'].split(chr(0))
-            if config.authenticate(request, username, password):
+        # Try to validate an existing session first.
+        sid = session.get('sid', None)
+        if sid is not None:
+            if config.validate_session(int(sid)):
+                return Response('Authenticated!', 200, {})
+            else:
+                # sid was invalid- either the session expired, was removed, or was bogus in the first place.
+                # Clear it from the session, then fall out to check for auth_credentials.
+                session.pop('sid')
+
+        # No existing session. See if credentials a present in the session to validate.
+        if 'auth_credentials' in session:
+            username, password = session.pop('auth_credentials').split(chr(0))
+            success, sid = config.authenticate(request, username, password)
+            session.modified = True
+            if success:
+                # Set the sid on the session to be persisted to subsequent requests.
+                session['sid'] = str(sid)
                 return Response('Authenticated!', 200, {})
         return Response('Authentication Required', 401, {})
-
 
     @app.route('/login', methods=['GET', 'POST'])
     def login():
         if request.method == 'POST':
-            session['nginxauth'] = request.form['username'] + chr(0) + request.form['password']
+            # Parse the credentials from the form and save them on the session to be processed later.
+            session['auth_credentials'] = request.form['username'] + chr(0) + request.form['password']
+
+            # Redirect to wherever the user was trying to go in the first place.
             return redirect(request.headers['Origin'] + request.form['target'])
         else:
+            # Present the login form, with a hidden target field to redirect with later.
             return render_template(template, target=request.headers['X-Target'])
-
 
     @app.route('/')
     def test_backend():
@@ -43,7 +61,7 @@ def server(config_module, port, secret_key, template):
 
     @app.errorhandler(500)
     def catch_server_errors(e):
-        logging.exception("Something awful happened!")
+        logging.exception("Exception Caught!")
 
     options = {}
 
@@ -59,7 +77,8 @@ def server(config_module, port, secret_key, template):
                 pass
 
     app.secret_key = options['secret_key']
-    app.run(host='0.0.0.0', port=options['port'])
+    app.run(host='0.0.0.0',
+            port=options['port'])
 
 if __name__ == '__main__':
     server()
